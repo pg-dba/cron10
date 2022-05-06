58fbae1a0f9e039f85578433518b37cf069e7782
Alexander.R <ar-mob@ya.ru>
2022-05-06 08:52:21 +0300

    all commit

--- c_send_pgbadger.sh
+++ c_send_pgbadger.sh
@@ -17,9 +17,9 @@ echo "[pgbadger]  Generate Weekly Report Finished"
 if [[ -v MAILSMTP ]]; then
 
 # MAILSMTP='smtp.inbox.ru:25'
-echo "<html>PostgreSQL on ${HOST} pgbadger Weekly Report<BR><BR>See Attachment</html>" | mutt -e 'set content_type = text/html' -e 'set from="${MAILLOGIN}"' \
-  -e 'set realname="${MAILFROM}"' -e 'set smtp_authenticators="login"' -e 'set smtp_url=smtp://"${MAILLOGIN}"@"${MAILSMTP}"' -e 'set smtp_pass="${MAILPWD}"' \
-  -e 'set ssl_starttls=yes' -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "${MAILTO}"
+echo "<html>PostgreSQL on ${HOST} pgbadger Weekly Report<BR><BR>See Attachment</html>" | mutt -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" \
+  -e "set realname=\"${MAILFROM}\"" -e 'set smtp_authenticators="login"' -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
+  -e 'set ssl_starttls=yes' -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "\"${MAILTO}\""
 
 fi
 
@@ -27,8 +27,8 @@ if [[ -v MAILSMTPURL ]]; then
 
 # MAILSMTPURL='smtp://10.42.161.197:25'
 echo "<html>PostgreSQL on ${HOST} pgbadger Weekly Report<BR><BR>See Attachment</html>" | mutt -e 'set ssl_starttls=no' -e 'set ssl_force_tls=no' \
-  -e 'set content_type = text/html' -e 'set from="${MAILLOGIN}"' -e 'set realname="${MAILFROM}"' -e 'set smtp_url="${MAILSMTPURL}"' \
-  -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- '"${MAILTO}"'"
+  -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
+  -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "\"${MAILTO}\""
 
 fi
 
