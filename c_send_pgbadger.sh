bd7a948d04d58a0bb61c623a1eaaed7cca845dd3
pg-dba <87299900+pg-dba@users.noreply.github.com>
2022-05-11 11:33:46 +0300

    Update c_send_pgbadger.sh

--- c_send_pgbadger.sh
+++ c_send_pgbadger.sh
@@ -19,7 +19,7 @@ if [[ -v MAILSMTP ]]; then
 # MAILSMTP='smtp.inbox.ru:25'
 echo "<html>PostgreSQL on ${HOST} pgbadger Weekly Report<BR><BR>See Attachment</html>" | mutt -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" \
   -e "set realname=\"${MAILFROM}\"" -e 'set smtp_authenticators="login"' -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
-  -e 'set ssl_starttls=yes' -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "\"${MAILTO}\""
+  -e 'set ssl_starttls=yes' -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "${MAILTO}"
 
 fi
 
@@ -28,7 +28,7 @@ if [[ -v MAILSMTPURL ]]; then
 # MAILSMTPURL='smtp://10.42.161.197:25'
 echo "<html>PostgreSQL on ${HOST} pgbadger Weekly Report<BR><BR>See Attachment</html>" | mutt -e 'set ssl_starttls=no' -e 'set ssl_force_tls=no' \
   -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
-  -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "\"${MAILTO}\""
+  -s 'PostgreSQL pgbadger Weekly Report' -a ${FileReport} -- "${MAILTO}"
 
 fi
 
