453ed8c1829701c91287fb5d23d641061607eda1
pg-dba <87299900+pg-dba@users.noreply.github.com>
2022-05-11 11:33:15 +0300

    Update c_send_locks.sh

--- c_send_locks.sh
+++ c_send_locks.sh
@@ -196,7 +196,7 @@ if [[ -v MAILSMTP ]]; then
 # MAILSMTP='smtp.inbox.ru:25'
 echo ${MSG} | mutt -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" \
     -e 'set smtp_authenticators="login"' -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" -e 'set ssl_starttls=yes' \
-    -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL Locks' "\"${MAILTO}\""
+    -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL Locks' "${MAILTO}"
 
 fi
 
@@ -204,7 +204,7 @@ if [[ -v MAILSMTPURL ]]; then
 
 # MAILSMTPURL='smtp://10.42.161.197:25'
 echo ${MSG} | mutt -e 'set ssl_starttls=no' -e 'set ssl_force_tls=no' -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" \
-    -e "set realname=\"${FROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" -s 'PostgreSQL Locks' "\"${MAILTO}\""
+    -e "set realname=\"${FROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" -s 'PostgreSQL Locks' "${MAILTO}"
 
 fi
 
