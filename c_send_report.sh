394a5af3d4d0c8676935e44b1ef0b79041c809f6
pg-dba <87299900+pg-dba@users.noreply.github.com>
2022-05-11 11:34:22 +0300

    Update c_send_report.sh

--- c_send_report.sh
+++ c_send_report.sh
@@ -18,7 +18,7 @@ if [[ -v MAILSMTP ]]; then
 # MAILSMTP='smtp.inbox.ru:25'
 cat ${FILEREPORT} | mutt -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" \
     -e 'set smtp_authenticators="login"' -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" -e 'set ssl_starttls=yes' \
-    -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL Daily Report' "\"${MAILTO}\""
+    -e 'set ssl_verify_dates=no' -e 'set ssl_verify_host=no' -s 'PostgreSQL Daily Report' "${MAILTO}"
 
 fi
 
@@ -26,7 +26,7 @@ if [[ -v MAILSMTPURL ]]; then
 
 # MAILSMTPURL='smtp://10.42.161.197:25'
 cat ${FILEREPORT} | mutt -e 'set ssl_starttls=no' -e 'set ssl_force_tls=no' -e 'set content_type = text/html' -e "set from=\"${MAILLOGIN}\"" \
-    -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" -s 'PostgreSQL Daily Report' "\"${MAILTO}\""
+    -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" -s 'PostgreSQL Daily Report' "${MAILTO}"
 
 fi
 
