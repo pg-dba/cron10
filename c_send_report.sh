7ebe5e092a2605862e4306138cdf41d63847c1e7
Alexander.R <ar-mob@ya.ru>
2022-05-30 09:43:29 +0300

    all commit

--- c_send_report.sh
+++ c_send_report.sh
@@ -3,16 +3,17 @@
 
 FILEREPORT='/cronwork/pg_profile_daily.html'
 
-echo "[pg_profile]  Generate pg_profile Daily Report"
-
 echo '<html><head><meta charset="utf-8"></head><body><p style="font-family:Monospace;font-size:10px"><a href="https://postgrespro.ru/docs/postgrespro/13/pgpro-pwr#PGPRO-PWR-SECTIONS-OF-A-REPORT">Описание разделов отчёта</a></p></body></html>' > ${FILEREPORT}
 PGPASSWORD=${PASSWORD} psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -P pager=off -qtc "SELECT profile.report_daily();" >> ${FILEREPORT}
+RC=$?
+echo "[pg_profile]  Generate pg_profile Daily Report. RC=${RC}"
+
 sed -i 's/<H2>Report sections<\/H2>/<H2><a NAME=report_sec>Report sections<\/H2>/' ${FILEREPORT}
 sed -i 's/<\/a><\/H3>/<\/a> <a HREF=#report_sec><button>up to contents<\/button><\/a><\/H3>/g' ${FILEREPORT}
-#chown 999:999 ${FILEREPORT}
 
-#echo "[pg_profile]  Reset pg_profile Stats"
-PGPASSWORD=${PASSWORD} psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "SELECT pg_stat_statements_reset();" 2>&1 | sed -n '1p' | ts '[pg_profile] ' 
+PGPASSWORD=${PASSWORD} psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "SELECT pg_stat_statements_reset();" 2>&1 | sed -n '1p' | ts '[pg_profile] '
+RC=$?
+echo "[pg_profile]  Reset pg_profile Stats. RC=${RC}"
 
 if [[ -v MAILSMTP ]]; then
 
@@ -23,9 +24,13 @@ cmdsend=$(echo mutt -e \"set content_type=text/html\" -e \"set send_charset=utf-
     -e \"set ssl_starttls=yes\" -e \"set ssl_verify_dates=no\" -e \"set ssl_verify_host=no\" -s \"PostgreSQL Daily Report\" ${MAILTO})
 #echo ${cmdsend}
 
-cat ${FILEREPORT} | mutt -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" -e "set from=\"${MAILLOGIN}\"" \
+rm -f /root/.muttdebug0
+
+cat ${FILEREPORT} | mutt -d3 -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" -e "set from=\"${MAILLOGIN}\"" \
     -e "set realname=\"${MAILFROM}\"" -e "set smtp_authenticators=\"login\"" -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
-    -e "set ssl_starttls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" -s "PostgreSQL Daily Report" ${MAILTO}
+    -e "set ssl_starttls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" -s "PostgreSQL Daily Report" ${MAILTO} | ts '[pg_profile] '
+RC=$?
+echo "[pg_profile]  Send pg_profile Daily Report. RC=${RC}"
 
 fi
 
@@ -37,10 +42,12 @@ cmdsend=$(echo mutt -e \"set ssl_starttls=no\" -e \"set ssl_force_tls=no\" -e \"
     -s \"PostgreSQL Daily Report\" ${MAILTO})
 #echo ${cmdsend}
 
-cat ${FILEREPORT} | mutt -e "set ssl_starttls=no" -e "set ssl_force_tls=no" -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" \
+rm -f /root/.muttdebug0
+
+cat ${FILEREPORT} | mutt -d3 -e "set ssl_starttls=no" -e "set ssl_force_tls=no" -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" \
     -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
-    -s "PostgreSQL Daily Report" ${MAILTO}
+    -s "PostgreSQL Daily Report" ${MAILTO} | ts '[pg_profile] '
+RC=$?
+echo "[pg_profile]  Send pg_profile Daily Report. RC=${RC}"
 
 fi
-
-echo "[pg_profile]  Send pg_profile Daily Report"
