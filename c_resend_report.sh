2349282f41e3f5f3bcc185a72d55480f1e198c0f
Alexander.R <ar-mob@ya.ru>
2022-06-01 12:59:52 +0300

    all commit

--- c_resend_report.sh
+++ c_resend_report.sh
@@ -5,18 +5,18 @@ FILEREPORT='/cronwork/pg_profile_daily.html'
 
 if [[ -v MAILSMTP ]]; then
 
-# MAILSMTP='smtp.inbox.ru:25'
-cmdsend=$(echo mutt -e \"set content_type=text/html\" -e \"set send_charset=utf-8\" -e \"set allow_8bit=yes\" \
+# MAILSMTP='smtp.inbox.ru:465'
+cmdsend=$(echo mutt -e \"set content_type=text/html\" -e \"set send_charset=utf-8\" -e \"set allow_8bit=yes\" -e \"set move=no\" \
     -e \"set from=\\\"${MAILLOGIN}\\\"\" -e \"set realname=\\\"${MAILFROM}\\\"\" \
-    -e \"set smtp_authenticators=\\\"login\\\"\" -e \"set smtp_url=smtp://\\\"${MAILLOGIN}\\\"@\\\"${MAILSMTP}\\\"\" -e \"set smtp_pass=\\\"${MAILPWD}\\\"\" \
-    -e \"set ssl_starttls=yes\" -e \"set ssl_verify_dates=no\" -e \"set ssl_verify_host=no\" -s \"PostgreSQL Daily Report\" ${MAILTO})
+    -e \"set smtp_authenticators=\\\"login\\\"\" -e \"set smtp_url=smtps://\\\"${MAILLOGIN}\\\"@\\\"${MAILSMTP}\\\"\" -e \"set smtp_pass=\\\"${MAILPWD}\\\"\" \
+    -e \"set ssl_starttls=yes\" -e \"set ssl_force_tls=yes\" -e \"set ssl_verify_dates=no\" -e \"set ssl_verify_host=no\" -s \"PostgreSQL Daily Report\" ${MAILTO})
 #echo ${cmdsend}
 
 rm -f /root/.muttdebug0
 
-cat ${FILEREPORT} | mutt -d3 -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" -e "set from=\"${MAILLOGIN}\"" \
-    -e "set realname=\"${MAILFROM}\"" -e "set smtp_authenticators=\"login\"" -e "set smtp_url=smtp://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
-    -e "set ssl_starttls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" -s "PostgreSQL Daily Report" ${MAILTO} | ts '[pg_profile]   '
+cat ${FILEREPORT} | mutt -d3 -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" -e "set move=no" -e "set from=\"${MAILLOGIN}\"" \
+    -e "set realname=\"${MAILFROM}\"" -e "set smtp_authenticators=\"login\"" -e "set smtp_url=smtps://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
+    -e "set ssl_starttls=yes" -e "set ssl_force_tls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" -s "PostgreSQL Daily Report" ${MAILTO} 2>&1 | ts '[pg_profile]   '
 RC=$?
 echo "[pg_profile]  Send Daily Report. RC=${RC}"
 
@@ -26,15 +26,15 @@ if [[ -v MAILSMTPURL ]]; then
 
 # MAILSMTPURL='smtp://10.42.161.197:25'
 cmdsend=$(echo mutt -e \"set ssl_starttls=no\" -e \"set ssl_force_tls=no\" -e \"set content_type=text/html\" -e \"set send_charset=utf-8\" -e \"set allow_8bit=yes\" \
-    -e \"set from=\\\"${MAILLOGIN}\\\"\" -e \"set realname=\\\"${MAILFROM}\\\"\" -e \"set smtp_url=\\\"${MAILSMTPURL}\\\"\" \
+    -e \"set move=no\" -e \"set from=\\\"${MAILLOGIN}\\\"\" -e \"set realname=\\\"${MAILFROM}\\\"\" -e \"set smtp_url=\\\"${MAILSMTPURL}\\\"\" \
     -s \"PostgreSQL Daily Report\" ${MAILTO})
 #echo ${cmdsend}
 
 rm -f /root/.muttdebug0
 
 cat ${FILEREPORT} | mutt -d3 -e "set ssl_starttls=no" -e "set ssl_force_tls=no" -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8bit=yes" \
-    -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
-    -s "PostgreSQL Daily Report" ${MAILTO} | ts '[pg_profile]   '
+    -e "set move=no" -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
+    -s "PostgreSQL Daily Report" ${MAILTO} 2>&1 | ts '[pg_profile]   '
 RC=$?
 echo "[pg_profile]  Send Daily Report. RC=${RC}"
 
