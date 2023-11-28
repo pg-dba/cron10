75f60710d704c50f37181fdd740762dc25fa0cee
Alexander <java.git@yandex.ru>
2023-11-28 12:56:12 +0300

    commit

--- c_log_switch.sh
+++ c_log_switch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# c_log_switch.sh
 PGPASSWORD=${PASSWORD} PGOPTIONS="-c geqo=off -c statement_timeout=5min -c client_min_messages=error" psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "select public.log_switch()::text;" 2>&1 | sed -n '1p' | ts '[pglog] ';
--- c_pg_version.sh
+++ c_pg_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# c_pg_version.sh
 PGPASSWORD=${PASSWORD} PGOPTIONS="-c geqo=off -c statement_timeout=5min -c client_min_messages=error" psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "SELECT LEFT(setting,5)::text as version FROM pg_settings where name='server_version';" 2>&1 | sed -n '1p' | ts '[PostgreSQL] ';
